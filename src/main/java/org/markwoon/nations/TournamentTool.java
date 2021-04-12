df14fa642ad7c548fb5e444932952fda3d300fc7
Mark Woon <markwoon@gmail.com>
2021-04-12 05:30:25 -0700

    fix: show app version in title

--- src/main/java/org/markwoon/nations/TournamentTool.java
+++ src/main/java/org/markwoon/nations/TournamentTool.java
@@ -1,5 +1,7 @@
 package org.markwoon.nations;
 
+import java.io.InputStream;
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,8 +14,17 @@ public class TournamentTool extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    String version = "";
+    try (InputStream in = getClass().getResourceAsStream("TournamentTool.properties")) {
+      if (in != null) {
+        Properties prop = new Properties();
+        prop.load(in);
+        version = " v" + prop.getProperty("version");
+      }
+    }
+
     Parent root = FXMLLoader.load(getClass().getResource("TournamentTool.fxml"));
-    primaryStage.setTitle("Nations Tournament Tool");
+    primaryStage.setTitle("Nations Tournament Tool" + version);
     primaryStage.setScene(new Scene(root, 600, 300));
 
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("marie_curie.png")));
