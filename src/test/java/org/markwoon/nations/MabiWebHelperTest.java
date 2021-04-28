8dea878a0a5aa3e0d476ac816bb4ff8bb96fdbd1
Mark Woon <markwoon@gmail.com>
2021-04-27 20:06:11 -0700

    test: update test

--- src/test/java/org/markwoon/nations/MabiWebHelperTest.java
+++ src/test/java/org/markwoon/nations/MabiWebHelperTest.java
@@ -15,18 +15,16 @@ class MabiWebHelperTest {
 
   @Test
   void foo() {
-    MabiWebHelper mabiWebHelper = new MabiWebHelper();
-
-    List<MabiWebHelper.NewGame> games = mabiWebHelper.buildTournamentGameList("foo", "A", 0);
+    List<MabiWebHelper.NewGame> games = MabiWebHelper.buildTournamentGameList("foo", "A", 0);
     assertEquals("foo - Group A - Game 101", games.get(0).name);
 
-    games = mabiWebHelper.buildTournamentGameList("foo", "B", 0);
+    games = MabiWebHelper.buildTournamentGameList("foo", "B", 0);
     assertEquals("foo - Group B - Game 204", games.get(3).name);
 
-    games = mabiWebHelper.buildTournamentGameList("foo", "B", 1);
+    games = MabiWebHelper.buildTournamentGameList("foo", "B", 1);
     assertEquals("foo - Group B1 - Game 201", games.get(0).name);
 
-    games = mabiWebHelper.buildTournamentGameList("foo", "D", 4);
+    games = MabiWebHelper.buildTournamentGameList("foo", "D", 4);
     assertEquals("foo - Group D4 - Game 448", games.get(11).name);
 
     /*
